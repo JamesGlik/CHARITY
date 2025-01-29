24e1d7cacc27db369820be832a3575453fd73d6e
JamesGlik <sadoiani9900@gmail.com>
2025-01-29 16:50:42 +0400

    fixed buttons

--- my-app/components/atoms/CharityButton/CharityButton.tsx
+++ my-app/components/atoms/CharityButton/CharityButton.tsx
@@ -4,7 +4,7 @@ import { CharityButtonProps } from '../../../types/CharityButtonProps/CharityBut
 const CharityButton = ({ text }: CharityButtonProps) => {
   return (
     <button
-      className="px-[28px] py-[13px] w-[202px] text-[#7E8AB8] text-[14px] font-semibold rounded-[24px] transition-all duration-300 ease-in-out hover:text-[#FFFFFF] hover:bg-[#7E8AB8] border border-[#7E8AB8] "
+      className="px-[40px] py-[15px] text-[#7E8AB8] text-[14px] font-semibold rounded-[24px] transition-all duration-300 ease-in-out hover:text-[#FFFFFF] hover:bg-[#7E8AB8] border border-[#7E8AB8] "
     >
       {text}
     </button>
