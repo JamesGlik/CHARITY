787ba66f27bc7a4b83f0445a6b467cfef82942dc
Nika Bostoganashvili <151997372+B0sto@users.noreply.github.com>
2025-02-02 00:58:50 +0400

    added stories section

---
+++ my-app/components/organisms/StoriesSection/StoriesSection.tsx
@@ -0,0 +1,28 @@
+import QuoteSlider from "@/components/molecules/QuoteSwiper/QuoteSwiper";
+import StoriesIcon from "@/public/StoriesIcon";
+import React from "react";
+
+const StoriesSection = () => {
+  return (
+    <div className="bg-[#f3f2e7]">
+      <div className="flex flex-col gap-y-[69px]">
+        <div className="flex flex-col gap-y-[17px] justify-center items-center">
+          <p className="text-[#E29F72] font-rowdies font-light leading-[32px] tracking-[3px] uppercase">
+            child stories
+          </p>
+          <h3 className="text-[#3A1700] font-rowdies text-[56px] font-light leading-[64px] tracking-[-2px]">
+            Read Stories
+          </h3>
+        </div>
+        <div className="relative">
+          <QuoteSlider />
+          <div className="absolute left-1/2 -top-7 transform -translate-x-1/2 z-[1]">
+            <StoriesIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StoriesSection;
