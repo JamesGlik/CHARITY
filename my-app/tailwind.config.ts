d2918368fcd94b9d322bac8d20b4dd4aaccf9225
JamesGlik <sadoiani9900@gmail.com>
2025-02-03 00:43:44 +0400

    fixed some bugs

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -14,8 +14,15 @@ export default {
       },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
-        rowdies: ["Rowdies", "sans-serif"]
-      }
+        rowdies: ["Rowdies", "sans-serif"],
+      },
+      screens: {
+        sm: { max: "639px" }, // Small screens (max-width: 639px)
+        md: { max: "767px" }, // Medium screens (max-width: 767px)
+        lg: { max: "1023px" }, // Large screens (max-width: 1023px)
+        xl: { max: "1279px" }, // Extra large screens (max-width: 1279px)
+        "2xl": { max: "1535px" }, // 2XL screens (max-width: 1535px)
+      },
     },
   },
   plugins: [],
