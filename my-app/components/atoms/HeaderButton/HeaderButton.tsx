2cc44f3b15c6ffcb05f024b9fcfb24a4333f4466
Nika Bostoganashvili <151997372+B0sto@users.noreply.github.com>
2025-02-03 16:53:56 +0400

    fixed bugs and added burger menu

--- my-app/components/atoms/HeaderButton/HeaderButton.tsx
+++ my-app/components/atoms/HeaderButton/HeaderButton.tsx
@@ -1,10 +1,10 @@
-import { Button } from '../../../types/HeaderButtonProps/HeaderButtonProps';
+import { Button } from "../../../types/HeaderButtonProps/HeaderButtonProps";
 
 const HeaderButton = ({ button }: Button) => {
   return (
-    <div className='font-rowdies text-[14px] uppercase rounded-[24px] border border-[rgba(126,138,184,0.30)] px-[35px] py-[15px] text-[#7E8AB8] cursor-pointer'>
+    <button className="px-[35px] py-[15px] uppercase border-[1px] border-[rgba(126, 138, 184, 0.30)] rounded-[24px] cursor-pointer font-rowdies text-[#7E8AB8] hover:bg-[#7E8AB8] hover:text-[#FFF] transition-all">
       {button}
-    </div>
+    </button>
   );
 };
 
--- my-app/components/molecules/Header/Header.tsx
+++ my-app/components/molecules/Header/Header.tsx
@@ -3,9 +3,11 @@ import HeaderText from "@/components/atoms/HeaderComp/HeaderText";
 import HeaderImage from "@/components/atoms/HeaderImage/HeaderImage";
 import HeaderLogo from "@/components/atoms/HeaderLogo/HeaderLogo";
 import BigRectangle from "@/public/BigRectangle";
+import BurgerMenuIcon from "@/public/BurgerMenuIcon";
 import Oval from "@/public/Oval";
 import Rectangle from "@/public/Rectangle";
 import React from "react";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 const Header = () => {
   return (
@@ -14,17 +16,17 @@ const Header = () => {
         <Rectangle />
       </div>
       <div className="absolute top-[150px]">
-        <Oval/>
+        <Oval />
       </div>
-      <div className="absolute right-[630px] top-[-40px]">
-        <BigRectangle/>
+      <div className="absolute right-[620px] top-[-40px] lg:hidden">
+        <BigRectangle />
       </div>
-      <div className="flex items-center gap-x-[58px] pl-[16px] pt-[16px] pr-[16px]">
+      <div className="flex items-center gap-x-[58px] pl-[16px] pt-[16px] pr-[16px] lg:justify-between">
         <div>
           <HeaderLogo logo="CHA" />
         </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex gap-x-[45px]">
+        <div className="flex items-center justify-between w-full lg:items-end lg:w-auto">
+          <div className="flex gap-x-[45px] lg:hidden">
             <HeaderText text="About" />
             <HeaderText text="Causes" />
             <HeaderText text="Services" />
@@ -32,26 +34,31 @@ const Header = () => {
             <HeaderText text="Blog" />
           </div>
 
-          <div className="w-full md:w-auto">
-            <HeaderButton button="contact" />
+          <div>
+            <div className="lg:hidden">
+              <HeaderButton button="contact"/>
+            </div>
+            <div className="hidden lg:block">
+              <BurgerMenu/>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-between px-[135px] items-center">
-        <div className="flex flex-col gap-y-[25px] w-[396px] justify-center items-start">
-          <h1 className="text-[72px] font-rowdies text-[#3A1700] font-light leading-[80px] tracking-[-3px]">
-            Giving help to those who need it
+      <div className="flex px-[135px] justify-between items-center xl:px-[50px] lg:flex-col-reverse">
+        <div className="flex flex-col gap-y-[25px] justify-center items-start">
+          <h1 className="text-[72px] font-rowdies text-[#3A1700] font-light leading-[80px] tracking-[-3px] z-[1] lg:text-[56px]">
+            Giving help <br /> to those who <br /> need it
           </h1>
           <p className="font-manrope leading-[32px] text-[rgba(58, 23, 0, 0.50)]">
-            It involves giving money, goods or time and effort to those who need
+            It involves giving money, goods or time and effort <br /> to those who need
             it.
           </p>
-          <button className="text-[#FFF] font-rowdies uppercase rounded-[24px] bg-[#7E8AB8] px-[32px] py-[16px]">
+          <button className="text-[#FFF] font-rowdies uppercase rounded-[24px] bg-[#7E8AB8] px-[32px] py-[16px] hover:bg-[#FFF] hover:text-[#7E8AB8] transition-all">
             make a donation
           </button>
         </div>
-        <div className="relative">
+        <div className="z-[1]">
           <HeaderImage />
         </div>
       </div>
