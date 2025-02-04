ab02ffe987dbcc6552f5ccef01cf703e56fc2679
JamesGlik <sadoiani9900@gmail.com>
2025-02-04 17:54:49 +0400

    made media query
    added media query for charity section

--- my-app/components/organisms/CharitySection/CharitySection.tsx
+++ my-app/components/organisms/CharitySection/CharitySection.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import ContentBlock from '../../molecules/ContentBlock/ContentBlock';
+import React from "react";
+import ContentBlock from "../../molecules/ContentBlock/ContentBlock";
 
 const CharitySection = () => {
   return (
     <section
-      className="w-full h-[840px] bg-cover bg-center flex items-center justify-between px-[135px]"
-      style={{ backgroundImage: `url('/charity_bg_image.svg')` }}
+      className="w-full h-[840px] bg-cover bg-center flex items-center justify-between px-[135px] sm:flex-col sm:h-auto sm:px-[40px] 
+                 bg-[url('/charity_bg_image.svg')] sm:bg-[url('/charity_media.svg')]"
     >
+      <img
+        src="/charity_image.svg"
+        alt="Charity Image"
+        className="object-contain rounded-lg sm:h-[720px] sm:mt-[-160px] sm:w-full"
+      />
       <ContentBlock
         heading="Giving help to those who need it"
         body="Charity is the act of giving help to those in need of it. It is a humanitarian act."
         buttonText="Make a Donation"
-      />
-      <img
-        src="/charity_image.svg"
-        alt="Right Side Image"
-        className="object-contain rounded-lg"
       />
     </section>
   );
