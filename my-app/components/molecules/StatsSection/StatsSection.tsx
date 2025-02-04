7975436c223280aa5fcb7a26f15ae49818fe8714
JamesGlik <sadoiani9900@gmail.com>
2025-02-05 00:41:39 +0400

    fixed some bugs
    fixed some bugs, added wrap and justify center

--- my-app/components/molecules/StatsSection/StatsSection.tsx
+++ my-app/components/molecules/StatsSection/StatsSection.tsx
@@ -5,7 +5,7 @@ const StatsSection = ({ stats, title }: StatsSectionProps) => {
     <div className="mt-[327px] sm:mt-[151px]">
       <h2 className="text-[24px] font-semibold text-[#3A1700] text-center mb-8 mx-auto w-[55%] sm:w-[85%] ">{title}</h2>
 
-      <div className="flex sm:flex-wrap justify-between gap-8 mt-[42px] sm:justify-center">
+      <div className="flex sm:flex-wrap lg:flex-wrap lg:justify-center justify-between gap-8 mt-[42px] sm:justify-center">
         {stats.map((stat, index) => (
           <div key={index} className="bg-white p-8 rounded-lg shadow-lg text-center flex items-center w-[370px] sm:w-[329px]">
             <h3 className="text-[56px] font-bold text-gray-800">{stat.number}</h3>
