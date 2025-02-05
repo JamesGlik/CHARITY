1f7e0dc29d97ffc086c8320fbcd9892a99caff91
JamesGlik <sadoiani9900@gmail.com>
2025-02-05 16:01:12 +0400

    fixed charity section
    fixed charity section image not wrapping

--- my-app/components/molecules/Newsletter/Newsletter.tsx
+++ my-app/components/molecules/Newsletter/Newsletter.tsx
@@ -5,7 +5,7 @@ import LatestButton from '@/components/atoms/LatestButton/LatestButton';
 function Newsletter() {
   return (
     <div className="pt-8 w-full sm:w-[329px] justify-center">
-      <div className="bg-[#F3F2E7] py-[40px] px-[48px] rounded-[12px] text-left flex justify-between sm:flex-col sm:items-start sm:px-[28px]">
+      <div className="bg-[#F3F2E7] py-[40px] px-[48px] rounded-[12px] text-left flex justify-between sm:flex-col lg:flex-col sm:items-start sm:px-[28px]">
         <div className="sm:w-full text-left"> 
           <h3 className="text-[40px] text-[#3A1700] sm:text-[32px] font-rowdies">Newsletter</h3>
           <p className="text-[#3A170080] font-manrope">
